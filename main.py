502cadf7d50cf1e78402d9ead5bbd1d6422faef6
Tom Wismar <tomjwismar@gmail.com>
2017-03-29 20:55:09 -0500

    added page header and footer

--- main.py
+++ main.py
@@ -15,10 +15,31 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>Sign Up</h1>
+"""
+
+page_footer = """
+</body>
+</html>
+"""
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
